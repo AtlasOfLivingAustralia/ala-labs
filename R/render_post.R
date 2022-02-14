8b58a5d8152a1710c8c44707605dc1be67db9f4a
Dax Kellie <dax.kellie@csiro.au>
2022-02-14 17:39:55 +1100

    Remove `renv` info

--- R/render_post.R
+++ R/render_post.R
@@ -1,28 +1,17 @@
+#------------------------------------------------------------------------------#
 # Render posts
 # (do not save over this file)
 # (fill in the correct information for the rmarkdown file you want to render)
+#------------------------------------------------------------------------------#
 
-## Step 1: Update your post's renv lockfile
-
-# If you have added or removed any packages to a post, update your renv snapshot
-# (no need to use `here` to locate the folder, refinery will do this automatically)
-refinery::renv_snapshot("name-of-post-folder")
-
-
-## Step 2: Restart R
-
-# If you were just working on a post, restart your R session (Ctl/Cmd + Shift + F10)
-
-
-## Step 3: Render your post
-
-# Now run the following code:
-# (I recommend copying and pasting below with correct details to render your post, then delete when finished)
+# |(I recommend copying the following code and pasting it again just below or in a new local R script. 
+# |Then you can fill in the correct details to render your post and delete when finished)
 
 library(galah)
 galah_config(email = "your-email@email.com")
 
 library(here)
-path <- here("_posts", "folder", "file-name.Rmd")
+path <- here("_posts", "name-of-post-folder", "file-name.Rmd")
 rmarkdown::render(path)
+
 
