2d603f4825ed29bb3951447feed74fdc79296a12
Martin Westgate <martinjwestgate@gmail.com>
2022-03-24 12:10:18 +1100

    hard-code non-functional relative links
    All links to author pages within posts were broken; simple fix is to paste `https://labs.ala.org.au/` in front. Ditto plotly navigation widget

--- R/ignition_sequence.R
+++ R/ignition_sequence.R
@@ -8,9 +8,8 @@ library(here)
 # add up to 3 recently-authored blog posts to each author profile
 source(here("R", "parse_author_articles.R"))
 
-# build
-rmarkdown::render_site()
-
-# post-calc
 # add tree-based blog navigation
 source(here("R", "plotly_navigation.R"))
+
+# build
+rmarkdown::render_site()
