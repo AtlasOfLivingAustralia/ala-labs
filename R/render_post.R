0155a92872b7ae97a760ef176739c438493117ba
Dax Kellie <dax.kellie@csiro.au>
2022-10-19 14:15:12 +1100

    Publish alpha hulls post
    * Remove posts list from people pages while a bug is causing rendering issues

--- R/render_post.R
+++ R/render_post.R
@@ -4,11 +4,11 @@
 # (fill in the correct information for the rmarkdown file you want to render)
 #------------------------------------------------------------------------------#
 
-# |(I recommend copying the following code and pasting it again just below or in a new local R script. 
+# |(I recommend copying the following code and pasting it again just below or in a new local R script.
 # |Then you can fill in the correct details to render your post and delete when finished)
 
 library(galah)
-galah_config(email = "your-email@email.com")
+galah_config(email = "your-email@email.com", verbose = FALSE)
 
 library(here)
 path <- here("_posts", "name-of-post-folder", "file-name.Rmd")
--- _site/R/ignition_sequence.R
+++ _site/R/ignition_sequence.R
@@ -6,11 +6,12 @@ library(here)
 
 # pre-calc
 # add up to 3 recently-authored blog posts to each author profile
-source(here("R", "parse_author_articles.R"))
+# source(here("R", "parse_author_articles.R"))
 
 # add tree-based blog navigation
 source(here("R", "plotly_navigation.R"))
 unlink(here("images", "plotly", "taxonomy_navigation_files"), recursive = TRUE)
 
 # build
-rmarkdown::render_site()
+rmarkdown::render_site(here::here())
+
