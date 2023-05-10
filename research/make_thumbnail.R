57bfdbc14223597a320c7755b3be25e1885fc25f
Dax Kellie <dax.kellie@csiro.au>
2023-05-10 13:31:43 +1000

    Update structure of make_thumbnail.R

--- research/make_thumbnail.R
+++ research/make_thumbnail.R
@@ -4,6 +4,24 @@
 # date: 8 May 2023
 # ---
 
+
+#------------ Edit these parameters for highlighted paper ---------------------#
+
+title <- "Changing farming climates: The future of growing peanuts"
+authors <- "Haerani et al. 2023"
+main_colour <- "#B7CD96" # use an ALA colour as your main colour
+text_colour <- "#754C06" # choose your own matching text colour: https://coolors.co/
+line_colour <- "#CB8752" # choose your own matching line colour
+
+
+# ALA colours to use as main colour
+ala_colours <- c("#E06E53", "#B8573E", "#667073", "#FFC557",
+                 "#B7CD96", "#6BDAD5", "#EEECEA", "#9E9E9F",
+                 "#222322", "#003A70", "#A191B2", "#691C32")
+
+
+#----------- Now run the code below to make and save thumbnail ----------------#
+
 # load packages
 library(ggtext)
 library(extrafont)
@@ -19,23 +37,12 @@ source(here("R", "functions_thumbnail.R"))
 font_add_google("Poppins", bold.wt = 500)
 showtext_auto(enable = TRUE)
 
-# ALA colours
-ala_colours <- c("#E06E53", "#B8573E", "#667073", "#FFC557",
-                 "#B7CD96", "#6BDAD5", "#EEECEA", "#9E9E9F",
-                 "#222322", "#003A70", "#A191B2", "#691C32")
-
-
-# Edit these parameters for highlighted paper
-title <- "Changing farming climates: The future of growing peanuts"
-authors <- "Haerani et al. 2023"
-main_colour <- "#B7CD96" # use an ALA colour as your main colour
-text_colour <- "#754C06" # choose your own matching text colour: https://coolors.co/
-line_colour <- "#CB8752" # choose your own matching line colour
-
-
 # Now run this function to generate and save thumbnail
 # Thumbnails are saved in ./research/images/thumbnail.png
 # Saved images will not be rendered until placed in specific paper folder
 make_paper_thumbnail(title, authors, main_colour, text_colour, line_colour)
 
-# When happy with thumbnail, copy/paste thumbnail image into specific paper folder
+
+#------------------------------------------------------------------------------#
+# When happy with thumbnail, copy/paste thumbnail.png into specific paper folder
+#------------------------------------------------------------------------------#
