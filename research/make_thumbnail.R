6711e2396553c49a1d57b280d6df847a3ed8cbec
oliviatorresan <olivia.torresan@csiro.au>
2024-04-04 15:47:43 +1100

    edit draft
    * text changes / focus change
    * thumbnail change
    * title change

--- research/make_thumbnail.R
+++ research/make_thumbnail.R
@@ -7,9 +7,9 @@
 
 #------------ Edit these parameters for highlighted paper ---------------------#
 
-title <- "Climate movements: Australia’s species going centre stage?"
+title <- "Open access habitat suitability maps of 10,633 Australian flora & fauna species"
 authors <- "Archibald et al. 2024"
-main_colour <- "#624580" # use an ALA colour as your main colour
+main_colour <- "#A191B2" # use an ALA colour as your main colour
 text_colour <- "#FFFFFF" # choose your own matching text colour: https://coolors.co/
 line_colour <- "#B7CD96" # choose your own matching line colour
 
